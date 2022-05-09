6de6db6e7040020c2773364acc350f309aa58f58
ZFenglin <913632394@qq.com>
2022-05-09 10:11:05 +0800

    feat: 文档更新

--- JavaScript/Test/代码手写/函数相关.js
+++ JavaScript/Test/代码手写/函数相关.js
@@ -53,3 +53,82 @@ sayName._call(p, 26, 'man')
 sayName._apply(p, [26, 'man'])
 const fn = sayName._bind(p, 26)
 fn('man')
+
+//////////  闭包
+// 1. 模块化
+const mp = (function ModulePerson(name) {
+    let age = 0
+    function growth() {
+        age++
+    }
+    function sayAge() {
+        console.log(name + ' is ' + age)
+    }
+    return {
+        growth,
+        sayAge
+    }
+})('zfl')
+mp.sayAge()
+mp.growth()
+mp.sayAge()
+
+// 2. 柯里化
+function curry() {
+    const fn = arguments[0]
+    const args = Array.prototype.slice.call(arguments, 1) || []
+    return function () {
+        const subArgs = args.slice(0)
+        for (let index = 0; index < arguments.length; index++) {
+            subArgs.push(arguments[index])
+        }
+        if (fn.length <= subArgs.length) {
+            return fn.apply(this, subArgs)
+        } else {
+            subArgs.unshift(fn)
+            return curry.apply(this, subArgs)
+        }
+    }
+}
+
+function curryES6(fn, ...args) {
+    return fn.length <= args.length ? fn(...args) : curry.bind(null, fn, ...args)
+}
+
+function sum(a, b, c) {
+    console.log(a + b + c)
+}
+const currySum = curry(sum, 1)
+const twoCurrySum = currySum(2)
+twoCurrySum(3)
+
+// 防抖和节流
+function debounce(fn, wait) {
+    let timer = null
+    return function () {
+        const context = this
+        const args = arguments
+        timer && clearTimeout(timer)
+        timer = setTimeout(() => {
+            fn.apply(context, args)
+        }, wait)
+    }
+}
+function throttle(fn, wait) {
+    let tagTime = Date.now()
+    return function () {
+        const context = this
+        const args = arguments
+        const runTime = Date.now()
+        if (runTime - tagTime >= wait) {
+            tagTime = Date.now()
+            fn.apply(context, args)
+        }
+    }
+}
+
+function scrollEvent(type) {
+    console.log('scrollEvent', type)
+}
+document.addEventListener('scroll', debounce(() => scrollEvent('debounce'), 1000))
+document.addEventListener('scroll', throttle(() => scrollEvent('throttle'), 1000))
